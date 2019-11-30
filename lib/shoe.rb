194c4bcd40f204a8bc89b6098e9ef54c2ced46d1
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-11-30 19:04:48 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,14 @@
-# Make your shoe class here!
+class Shoe
+  def initialize(shoe)
+    @shoe = shoe
+  end
+  def shoe
+    @shoe
+  end
+  def brand=(brand)
+    @brand = brand
+  end
+#  def brand
+#    @brand
+#  end
+end
