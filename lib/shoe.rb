e4025f3c9ce37fd91d53ae6e024e4565d5a6b33d
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-11-30 19:15:58 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,8 +1,8 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-  def initialize(shoe)
-    @shoe = shoe
+  def initialize(brand)
+    @brand = brand
   end
   def cobble
     puts "Your shoe is as good as new!"
