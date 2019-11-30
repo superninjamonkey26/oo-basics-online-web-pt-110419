cf0fc0337ff8e9979172f44bbaa8cc634f2a70c3
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-11-30 19:09:07 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,19 +1,3 @@
-#class Shoe
-#  def initialize(shoe)
-#    @shoe = shoe
-#  end
-#  def shoe
-#    @shoe
-#  end
-#  def brand=(brand)
-#    @brand = brand
-#  end
-#  def brand
-#    @brand
-#  end
-#end
-
-
 class Shoe
   attr_accessor :brand, :color, :size, :material, :condition
   def initialize(shoe)
